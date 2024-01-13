f0688961c33725a9df97555eeee6634ebc38ee76
talhachaudharyy <chaudharytalhaofficial@gmail.com>
2024-01-14 02:46:19 +0500

    my mini commit

---
+++ components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter, AiFillFacebook } from 'react-icons/ai'
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+      <p> 2023 TC Store All rights reserved</p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+        < AiFillFacebook/>
+
+      </p>
+    </div>
+  )
+}
+
+export default Footer
